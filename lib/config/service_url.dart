3d8ff678065f3e6aa2820a7b2ce1e2aefb38a1b5
hdq <haqsir@163.com>
2019-08-19 22:48:35 +0800

    添加请求文件，同意管理获取数据

---
+++ lib/config/service_url.dart
@@ -0,0 +1,4 @@
+const serviceUrl= 'xxxxxx';
+const servicePath={
+  'homePageContext': serviceUrl+'wxmini/homePageContent', 
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,54 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
-import '../config/httpHeaders.dart';
+import '../service/service_method.dart';
+
 
 class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  String showText='还没有请求数据';
+  String homePageContent='正在获取数据';
+
+  @override
+  void initState() {
+    getHomePageContent().then((val){
+      setState((){
+        homePageContent=val.toString();
+      });
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
        child: Scaffold(
-         appBar: AppBar(title: Text('请求远程数据'),),
+         appBar: AppBar(title: Text('百姓生活+')),
          body: SingleChildScrollView(
-           child: Column(
-             children: <Widget>[
-               RaisedButton(
-                 onPressed: _juejin,
-                 child: Text('请求数据'),
-               ),
-               Text(showText)
-             ],
-           ),
+           child: Text(homePageContent),  
          ),
        ),
     );
   }
-
-  void _juejin(){
-    print('开始向极客时间请求数据..................');
-    getHttp().then((val){
-      setState(() {
-       showText=val['data'].toString();
-      });
-    });
-  }
-
-
-  Future getHttp()async{
-    try{
-      Response response;
-      Dio dio = new Dio();
-      dio.options.headers= httpHeaders;
-      response =await dio.get("https://time.geekbang.org/serv/v1/column/newAll");
-      print(response);
-      return response.data;
-    }catch(e){
-      return print(e);
-    }
-  }
-
 }
