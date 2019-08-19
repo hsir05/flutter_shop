c9ac33209ffb951b2e13a0bd9816040057399034
hdq <haqsir@163.com>
2019-08-19 21:45:16 +0800

    flutter dio使用

---
+++ lib/pages/dio.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DioDemo extends StatefulWidget {
+  @override
+  _DioDemoState createState() => _DioDemoState();
+}
+
+class _DioDemoState extends State<DioDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
