acd8c43d5519f1bb8fabbee5b0e57a3ac450ed51
hdq <haqsir@163.com>
2019-08-20 23:00:49 +0800

    导航展示

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
-import 'dart:convert';
+// import 'dart:convert';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
@@ -32,12 +32,15 @@ class _HomePageState extends State<HomePage> {
            future: getHomePageContent(),
            builder: (context, snapshot) {
              if(snapshot.hasData) {
-              //  var data = json.decode(snapshot.data.toString());
-               var data = snapshot.data['data']['focus'];
-               List <Map> swiperDataList = (data as List).cast();
+
+               var data = snapshot.data['data'];
+               List <Map> swiperDataList = (data['focus'] as List).cast();
+              List <Map> navigatorList = (data['kingKongAreaV4'] as List).cast();
+
                return Column(
                  children: <Widget>[
-                   SwiperDiy(swiperDataList: swiperDataList,)
+                   SwiperDiy(swiperDataList: swiperDataList,),
+                   TopNavigator(navigatorList:navigatorList),
                  ],
                );
              } else {
@@ -50,6 +53,7 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+// 轮播
 class SwiperDiy extends StatelessWidget {
   final List swiperDataList;
   SwiperDiy({Key key,this.swiperDataList}):super(key:key);
@@ -67,6 +71,39 @@ class SwiperDiy extends StatelessWidget {
         itemCount: swiperDataList.length,
         pagination: new SwiperPagination(),
         autoplay: true,
+      ),
+    );
+  }
+}
+
+// 导航 kingKongAreaV4
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+  TopNavigator({Key key, this.navigatorList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding: EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(4.0),
+        children: navigatorList.map((item){
+          return _gridViewItemUI(context, item);
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _gridViewItemUI(BuildContext context, item) {
+    return InkWell(
+      onTap: () {print('点击了导航');},
+      child: Column(
+        children: <Widget>[
+          Image.network(item['picUrls'][0], width: ScreenUtil().setWidth(95)),
+          Text(item['title'])
+        ],
       ),
     );
   }
