6b3c72c1b0ab4120e9e88a57ce3b3ddd86825d20
hdq <haqsir@163.com>
2019-10-27 00:18:42 +0800

    图片选择功能ok

--- lib/pages/goldenCard_page.dart
+++ lib/pages/goldenCard_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class GoldenCard extends StatefulWidget {
   @override
@@ -12,7 +13,7 @@ class GoldenCard extends StatefulWidget {
 class _GoldenCardState extends State<GoldenCard> {
 
   List _imageList = []; //图片列表
-  String _imageStr = 'assets/images/membg.png';
+  String _imageStr = 'assets/images/avatar.png';
 
   int _photoIndex = 0; //选择拍照还是相册的索引
   List _actionSheet = [
@@ -22,6 +23,7 @@ class _GoldenCardState extends State<GoldenCard> {
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
     return Scaffold(
         appBar: AppBar(
             backgroundColor: Color.fromRGBO(180, 40, 45, 1),
@@ -32,7 +34,9 @@ class _GoldenCardState extends State<GoldenCard> {
                 }),
             title: Text("回馈金卡"), 
             centerTitle: true),
-        body: SingleChildScrollView(
+        body: Container(
+          width: ScreenUtil().setWidth(750),
+          child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
@@ -45,45 +49,50 @@ class _GoldenCardState extends State<GoldenCard> {
               avatar(),
 
               _imageList.isNotEmpty ? Wrap(
-                  spacing: 10.0,
+                  spacing: 4.0,
+                  crossAxisAlignment: WrapCrossAlignment.center,
                   children: _getImageList(),
                 ) : Text("暂无内容")
 
             ],
           ),
-        ));
+        ),
+        )
+        );
   }
 
   Widget avatar() {
-    return Container(
-      height: 50.0,
-      padding: EdgeInsets.only(top: 25, bottom: 25),
-      decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(_imageStr),
-            fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        _getActionSheet();
+      },
+      child: Container(
+            width: 70.0,
+            height: 70.0,
+            child: CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage(_imageStr)),
           ),
-        ),
-      child:Container(width:200.0, height: 300.0,child: Text('data'),));
-  }
+        );
+      }
 
   //图片列表的刻画
   List<Widget> _getImageList() {
     return _imageList.map((img) {
-      return ClipRRect(
-        borderRadius: BorderRadius.circular(10.0),
+      return Container(
+        margin: EdgeInsets.only(top: 10.0),
         child: Stack(
           children: <Widget>[
-            
-            Image.file( img, fit: BoxFit.cover, width: 100.0, height: 70.0, ),
-            
+            Container(
+              child: Image.file( img, fit: BoxFit.cover, width: ScreenUtil().setWidth(350), height: ScreenUtil().setHeight(300), ),
+            ),
             Positioned(
               right: 5.0,
               top: 5.0,
               child: GestureDetector(
                 child: Container(
-                    width: 15.0,
-                    height: 15.0,
+                    width: 20.0,
+                    height: 20.0,
                     color: Colors.lightBlue,
                     child: Icon(
                       Icons.close,
@@ -113,6 +122,7 @@ class _GoldenCardState extends State<GoldenCard> {
     if (image != null) {
       setState(() {
         _imageList.add(image);
+        // _imageStr = image;
       });
     }
   }
